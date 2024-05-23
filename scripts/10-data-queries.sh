95341c48e3fa65a63d843cd67c7a1bbe133ded2a
Bruce W. Herr II <bherr@indiana.edu>
2024-05-23 13:59:05 -0400

    Added query and data for hra pop mapped to the hra visualization

--- scripts/10-data-queries.sh
+++ scripts/10-data-queries.sh
@@ -3,5 +3,8 @@ shopt -s extglob
 set -ev
 
 ENDPOINT=https://lod.humanatlas.io/sparql
+HRAPOP_ENDPOINT=http://localhost:8080/blazegraph/namespace/kb/sparql
 
 src/cli/sparql-select.sh $ENDPOINT queries/sparql/data/digital-objects-per-organ.rq | csvsort -c organ > data/data/digital-objects-per-organ.csv
+
+src/cli/sparql-select.sh $HRAPOP_ENDPOINT queries/sparql/experimental-data/hra-pop-mapped-to-hra.rq > data/experimental-data/hra-pop-mapped-to-hra.csv
