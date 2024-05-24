5e8b85f7ccd03a9ec58b97fc2506741916ee9686
Bruce W. Herr II <bherr@indiana.edu>
2024-05-24 10:29:14 -0400

    Initital HRA growth query and results

--- scripts/10-data-queries.sh
+++ scripts/10-data-queries.sh
@@ -3,8 +3,6 @@ shopt -s extglob
 set -ev
 
 ENDPOINT=https://lod.humanatlas.io/sparql
-HRAPOP_ENDPOINT=http://localhost:8080/blazegraph/namespace/kb/sparql
 
 src/cli/sparql-select.sh $ENDPOINT queries/sparql/data/digital-objects-per-organ.rq | csvsort -c organ > data/data/digital-objects-per-organ.csv
-
-src/cli/sparql-select.sh $HRAPOP_ENDPOINT queries/sparql/experimental-data/hra-pop-mapped-to-hra.rq > data/experimental-data/hra-pop-mapped-to-hra.csv
+src/cli/sparql-select.sh $ENDPOINT queries/sparql/data/hra-growth.rq > data/data/hra-growth.csv
---
+++ scripts/10-experimental-data-queries.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+shopt -s extglob
+set -ev
+
+ENDPOINT=https://lod.humanatlas.io/sparql
+HRAPOP_ENDPOINT=http://localhost:8080/blazegraph/namespace/kb/sparql
+
+src/cli/sparql-select.sh $HRAPOP_ENDPOINT queries/sparql/experimental-data/hra-pop-mapped-to-hra.rq > data/experimental-data/hra-pop-mapped-to-hra.csv
