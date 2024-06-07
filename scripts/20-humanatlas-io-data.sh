1ab210ea1b360946c10ff38c13ee263b852218f6
Bruce W. Herr II <bherr@indiana.edu>
2024-06-07 12:49:32 -0400

    Add code to generate as-per-organ.csv and use HRA v2.1 data

--- scripts/20-humanatlas-io-data.sh
+++ scripts/20-humanatlas-io-data.sh
@@ -3,9 +3,12 @@ shopt -s extglob
 set -ev
 
 ENDPOINT=https://lod.humanatlas.io/sparql
+# ENDPOINT=http://localhost:8080/blazegraph/namespace/kb/sparql
 
 ./src/cli/sparql-select.sh $ENDPOINT queries/sparql/humanatlas.io/ref-organs.rq > data/humanatlas.io/ref-organs.csv
 csvjson -I data/humanatlas.io/ref-organs.csv | yq -y "$(cat queries/sparql/humanatlas.io/ref-organs.jqx)" > data/humanatlas.io/ref-organs.yaml
 
 ./src/cli/sparql-select.sh $ENDPOINT queries/sparql/humanatlas.io/ftu-illustrations.rq > data/humanatlas.io/ftu-illustrations.csv
 csvjson -I data/humanatlas.io/ftu-illustrations.csv | yq -y "$(cat queries/sparql/humanatlas.io/ftu-illustrations.jqx)" > data/humanatlas.io/ftu-illustrations.yaml
+
+./src/cli/sparql-select.sh $ENDPOINT queries/sparql/humanatlas.io/as-per-organ.rq | csvformat > data/humanatlas.io/as-per-organ.csv
