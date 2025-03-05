d6bcf676f63541afacfba897bfd899a0c9f8f8af
Bruce W. Herr II <bherr@iu.edu>
2025-03-05 11:26:34 -0500

    Update atlas datasets query

--- scripts/10-experimental-data-queries.sh
+++ scripts/10-experimental-data-queries.sh
@@ -5,5 +5,5 @@ set -ev
 ENDPOINT=https://lod.humanatlas.io/sparql
 HRAPOP_ENDPOINT=http://localhost:8080/blazegraph/namespace/kb/sparql
 
-# src/cli/sparql-select.sh $HRAPOP_ENDPOINT queries/sparql/experimental-data/hra-pop-mapped-to-hra.rq > data/experimental-data/hra-pop-mapped-to-hra.csv
+src/cli/sparql-select.sh $HRAPOP_ENDPOINT queries/sparql/experimental-data/hra-pop-mapped-to-hra.rq > data/experimental-data/hra-pop-mapped-to-hra.csv
 src/cli/sparql-select.sh $HRAPOP_ENDPOINT queries/sparql/experimental-data/hra-pop-atlas-mapped-to-hra.rq > data/experimental-data/hra-pop-atlas-mapped-to-hra.csv
