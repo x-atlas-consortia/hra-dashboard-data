b79399143d907b1d120503d93d7c216c75e6c840
Bruce W. Herr II <bherr@iu.edu>
2025-03-26 15:00:24 -0400

    Update usage

--- scripts/10-usage-queries.sh
+++ scripts/10-usage-queries.sh
@@ -3,6 +3,8 @@ shopt -s extglob
 set -ev
 
 node src/usage/global-portal-usage.js
+sleep 5
+
 node src/cli/athena-query.js queries/athena/usage/hra-requests.sql data/usage/hra-requests.csv
 node src/cli/athena-query.js queries/athena/usage/raw-referrers.sql data/usage/raw-referrers.csv
 node src/cli/athena-query.js queries/athena/usage/time-period.sql data/usage/time-period.csv
