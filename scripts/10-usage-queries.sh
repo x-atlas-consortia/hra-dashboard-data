e5445aae70665bcf5f2f54dffbaf5efeb057729d
Bruce W. Herr II <bherr@iu.edu>
2025-04-04 08:58:39 -0400

    Update usage

--- scripts/10-usage-queries.sh
+++ scripts/10-usage-queries.sh
@@ -5,7 +5,6 @@ set -ev
 node src/usage/global-portal-usage.js
 sleep 5
 
-node src/cli/athena-query.js queries/athena/usage/hra-requests.sql data/usage/hra-requests.csv
 node src/cli/athena-query.js queries/athena/usage/raw-referrers.sql data/usage/raw-referrers.csv
 node src/cli/athena-query.js queries/athena/usage/time-period.sql data/usage/time-period.csv
 node src/usage/top-referrers.js
@@ -13,3 +12,6 @@ node src/cli/athena-query.js queries/athena/usage/total-requests.sql data/usage/
 
 node src/cli/athena-query.js queries/athena/usage/raw-referrer-requests.sql data/usage/raw-referrer-requests.csv
 node src/usage/top-users.js
+sleep 10
+
+node src/cli/athena-query.js queries/athena/usage/hra-requests.sql data/usage/hra-requests.csv
