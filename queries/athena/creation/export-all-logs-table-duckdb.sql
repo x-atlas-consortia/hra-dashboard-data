79189d385bae47d705b69a1052d9b22478ee506f
Bruce W. Herr II <bherr@iu.edu>
2025-08-07 16:46:36 -0400

    Update duckdb export for all_logs

---
+++ queries/athena/creation/export-all-logs-table-duckdb.sql
@@ -0,0 +1,15 @@
+INSTALL httpfs;
+
+CREATE OR REPLACE SECRET secret (
+    TYPE s3,
+    PROVIDER credential_chain
+);
+
+CREATE TABLE all_logs AS
+SELECT *
+FROM read_parquet('s3://hra-cdn-logs/all_logs/**/*',
+  hive_partitioning = true,
+  hive_types = {'site': VARCHAR, 'year': INT }
+);
+
+COPY all_logs TO 'hra-logs.parquet' (FORMAT parquet, COMPRESSION zstd);
