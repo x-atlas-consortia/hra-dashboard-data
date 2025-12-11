f8d4b821e982e4f4f050779edaf4aee6116f8a60
Bruce W. Herr II <bherr@iu.edu>
2025-12-11 14:44:12 -0500

    HRA v2.4 updates

--- scripts/10-data-queries.sh
+++ scripts/10-data-queries.sh
@@ -6,14 +6,16 @@ ENDPOINT=https://lod.humanatlas.io/sparql
 # ENDPOINT=http://localhost:8080/blazegraph/namespace/kb/sparql
 
 HRALIT_PUBS=https://api.figshare.com/v2/file/download/44343890
-SOPs="https://zenodo.org/api/communities/ef4a7af8-55bb-4304-9ea4-80c5186236c9/records?q=&sort=newest&page=1&size=50"
+SOPs1="https://zenodo.org/api/communities/ef4a7af8-55bb-4304-9ea4-80c5186236c9/records?q=&sort=newest&page=1&size=25"
+SOPs2="https://zenodo.org/api/communities/ef4a7af8-55bb-4304-9ea4-80c5186236c9/records?q=&sort=newest&page=2&size=25"
 
 src/cli/sparql-select.sh $ENDPOINT queries/sparql/data/digital-objects-per-organ.rq | csvsort -c organ > data/data/digital-objects-per-organ.csv
 
 curl -L $HRALIT_PUBS | zcat | csvcut -c pmid,pubyear | csvsort -c pubyear -r | csvformat > data/data/hra-growth.hra-lit.csv
 
 echo "doi,created" > data/data/hra-growth.sop.csv
-curl $SOPs | jq -r '.hits.hits[] | [.conceptdoi,.created] | @csv' | csvsort -c 2 -r | csvformat >> data/data/hra-growth.sop.csv
+curl $SOPs1 | jq -r '.hits.hits[] | [.conceptdoi,.created] | @csv' | csvsort -c 2 -r | csvformat >> data/data/hra-growth.sop.csv
+curl $SOPs2 | jq -r '.hits.hits[] | [.conceptdoi,.created] | @csv' | csvsort -c 2 -r | csvformat >> data/data/hra-growth.sop.csv
 
 src/cli/sparql-select.sh $ENDPOINT queries/sparql/data/hra-growth.atlas.rq > data/data/hra-growth.atlas.csv
 src/cli/sparql-select.sh $ENDPOINT queries/sparql/data/hra-growth.experimental.rq > data/data/hra-growth.experimental.csv
--- src/usage/sop-downloads.js
+++ src/usage/sop-downloads.js
@@ -1,7 +1,13 @@
-const API =
-  'https://zenodo.org/api/communities/ef4a7af8-55bb-4304-9ea4-80c5186236c9/records?q=&sort=newest&page=1&size=50';
+const API1 =
+  'https://zenodo.org/api/communities/ef4a7af8-55bb-4304-9ea4-80c5186236c9/records?q=&sort=newest&page=1&size=25';
+const API2 =
+  'https://zenodo.org/api/communities/ef4a7af8-55bb-4304-9ea4-80c5186236c9/records?q=&sort=newest&page=1&size=25';
 
-const results = await fetch(API).then((r) => r.json());
-const count = results.hits.hits.map((n) => n.stats.downloads).reduce((a, b) => a + b, 0);
+
+let results = await fetch(API1).then((r) => r.json());
+let count = results.hits.hits.map((n) => n.stats.downloads).reduce((a, b) => a + b, 0);
+
+results = await fetch(API2).then((r) => r.json());
+count += results.hits.hits.map((n) => n.stats.downloads).reduce((a, b) => a + b, 0);
 
 console.log(count);
