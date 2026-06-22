c8a56f4a7e6dacf0476963849d71d89d08435e29
Bruce W. Herr II <bherr@iu.edu>
2026-06-22 09:58:38 -0400

    Update to automatically put the right date for usage log export

--- scripts/11x-export-usage-logs.sh
+++ scripts/11x-export-usage-logs.sh
@@ -2,7 +2,8 @@
 
 duckdb -no-stdin -init queries/athena/creation/export-all-logs-table-duckdb.sql
 
+YESTERDAY=$(date -Idate -d "yesterday")
 mkdir -p scratch
-BASE="scratch/`date -Idate`_hra-logs"
+BASE="scratch/${YESTERDAY}_hra-logs"
 mv hra-logs.parquet ${BASE}.parquet
 mv hra-logs.csv.gz ${BASE}.csv.gz
